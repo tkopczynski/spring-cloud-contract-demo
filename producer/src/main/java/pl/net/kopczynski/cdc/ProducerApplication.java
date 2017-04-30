8b7d6e8e58bdc9f29627dc03726926f0e69e72fd
Tomasz Kopczynski <tomek@kopczynski.net.pl>
2017-04-30 21:23:36 +0200

    Initial commit

---
+++ producer/src/main/java/pl/net/kopczynski/cdc/ProducerApplication.java
@@ -0,0 +1,12 @@
+package pl.net.kopczynski.cdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+}
